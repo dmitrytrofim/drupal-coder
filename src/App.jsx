ace2aededac61b3d3c1848765421e25da0a26630
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-01-03 19:37:28 +0300

    +

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+ return (
+  <div className="wrapper">
+   <Header />
+   <main></main>
+   <Footer />
+  </div>
+ );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+ return <header></header>;
+}
+
+export default Footer;
