fd11eb552360e08314d8ae73a2678432e5c818a7
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-01-04 11:49:00 +0300

    +

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 function Header() {
- return <header className="header"></header>;
+ return (
+  <header className="page__header header">
+   <a href="#" className="header__logo">
+    <img src="./assets/img/svg/logo.svg" alt="" className="" />
+   </a>
+  </header>
+ );
 }
 
 export default Header;
