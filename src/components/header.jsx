bed12c2a4b68b828e7be89f92c510719526a3956
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-01-04 12:45:03 +0300

    +

---
+++ src/components/header.jsx
@@ -0,0 +1,53 @@
+function Header() {
+ return (
+  <header className="page__header header">
+   <div className="container">
+    <div className="header__container">
+     <a href="#" className="header__logo">
+      <img
+       src="./assets/img/svg/logo.svg"
+       alt=""
+       className="header__logo-img"
+      />
+     </a>
+     <nav className="header__nav">
+      <ul className="header__nav-list">
+       <li className="header__nav-item">
+        <a href="#" className="header__nav-link">
+         Поддержка сайтов
+        </a>
+       </li>
+       <li className="header__nav-item">
+        <a href="#" className="header__nav-link">
+         Тарифы
+        </a>
+       </li>
+       <li className="header__nav-item">
+        <a href="#" className="header__nav-link">
+         Наши работы
+        </a>
+       </li>
+       <li className="header__nav-item">
+        <a href="#" className="header__nav-link">
+         Отзывы
+        </a>
+       </li>
+       <li className="header__nav-item">
+        <a href="#" className="header__nav-link">
+         Контакты
+        </a>
+       </li>
+      </ul>
+     </nav>
+     <div className="header__group">
+      <a href="tel:+78002222673" className="header__phone">
+       8 800 222-26-73
+      </a>
+     </div>
+    </div>
+   </div>
+  </header>
+ );
+}
+
+export default Header;
