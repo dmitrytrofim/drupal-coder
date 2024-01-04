b6c8ebb912c60cec8c4557cd73a60e2c33fc11c6
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-01-04 17:30:30 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -268,11 +268,11 @@ function App() {
     <section className="main__tariffs tariffs">
      <div className="container">
       <div className="tariffs__container">
-       <h2 className="tariffs__title"></h2>
+       <h2 className="tariffs__title">Тарифы</h2>
        <div className="tariffs__box">
         <div className="tariffs__card">
          <div className="tariffs__card-head">
-          <p className="tariffs__card-name"></p>
+          <p className="tariffs__card-name">Стартовый</p>
           <p className="tariffs__card-cost">
            2000 <i>₽</i>
           </p>
@@ -280,12 +280,13 @@ function App() {
          </div>
          <hr className="tariffs__card-line" />
          <ul className="tariffs__card-list">
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
+          <li className="tariffs__card-item">Предоплата от 2 часов</li>
+          <li className="tariffs__card-item">Консультации и работы по SEO</li>
+          <li className="tariffs__card-item">Услуги дизайнера</li>
+          <li className="tariffs__card-item">Стандартное время реакции</li>
+          <li className="tariffs__card-item">
+           Неиспользованные оплаченные часы переносятся на следующий месяц
+          </li>
          </ul>
          <a href="#" className="tariffs__card-btn">
           Оставить заявку!
@@ -293,7 +294,7 @@ function App() {
         </div>
         <div className="tariffs__card">
          <div className="tariffs__card-head">
-          <p className="tariffs__card-name"></p>
+          <p className="tariffs__card-name">Бизнес</p>
           <p className="tariffs__card-cost">
            2000 <i>₽</i>
           </p>
@@ -301,12 +302,15 @@ function App() {
          </div>
          <hr className="tariffs__card-line" />
          <ul className="tariffs__card-list">
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
+          <li className="tariffs__card-item">Предоплата от 10 часов</li>
+          <li className="tariffs__card-item">Консультации и работы по SEO</li>
+          <li className="tariffs__card-item">Услуги дизайнера</li>
+          <li className="tariffs__card-item">
+           Высокое время реакции – до 2 рабочих дней
+          </li>
+          <li className="tariffs__card-item">
+           Неиспользованные часы не переносятся
+          </li>
          </ul>
          <a href="#" className="tariffs__card-btn">
           Оставить заявку!
@@ -314,26 +318,237 @@ function App() {
         </div>
         <div className="tariffs__card">
          <div className="tariffs__card-head">
-          <p className="tariffs__card-name"></p>
+          <p className="tariffs__card-name">VIP</p>
           <p className="tariffs__card-cost">
-           2000 <i>₽</i>
+           1800 <i>₽</i>
           </p>
           <p className="tariffs__card-descr">в час</p>
          </div>
          <hr className="tariffs__card-line" />
          <ul className="tariffs__card-list">
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
-          <li className="tariffs__card-item"></li>
+          <li className="tariffs__card-item">Предоплата от 100 часов</li>
+          <li className="tariffs__card-item">Консультации и работы по SEO</li>
+          <li className="tariffs__card-item">Услуги дизайнера</li>
+          <li className="tariffs__card-item">
+           Максимальное время реакции – в день обращения
+          </li>
+          <li className="tariffs__card-item">
+           Неиспользованные часы не переносятся
+          </li>
          </ul>
          <a href="#" className="tariffs__card-btn">
           Оставить заявку!
          </a>
         </div>
        </div>
+       <p className="tariffs__descr">
+        Вам не подходят наши тарифы? Оставьте заявку и мы предложим вам
+        индивидуальные условия!
+       </p>
+       <a href="#" className="tariffs__link">
+        Получить индивидуальный тариф
+       </a>
+      </div>
+     </div>
+    </section>
+    <section className="main__cases cases">
+     <div className="container">
+      <div className="cases__container">
+       <h2 className="cases__title">Последние кейсы</h2>
+       <div className="cases__box">
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-5.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+          <p className="cases__card-date">22.04.2019</p>
+          <p className="cases__card-text">
+           Эти слова совершенно справедливы, однако гипнотический рифф
+           продолжает паузный пласт.
+          </p>
+         </div>
+        </a>
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-7.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+         </div>
+        </a>
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-4.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+          <p className="cases__card-date">22.04.2019</p>
+         </div>
+        </a>
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-3.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+          <p className="cases__card-date">22.04.2019</p>
+         </div>
+        </a>
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-1.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+          <p className="cases__card-date">22.04.2019</p>
+          <p className="cases__card-text">
+           Эти слова совершенно справедливы, однако гипнотический рифф
+           продолжает паузный пласт.
+          </p>
+         </div>
+        </a>
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-6.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+         </div>
+        </a>
+        <a href="#" className="cases__card m-sep">
+         <img
+          className="cases__card-visual"
+          src="/assets/img/case-2.webp"
+          alt=""
+          loading="lazy"
+         />
+         <div className="cases__card-info">
+          <p className="cases__card-title">
+           Настройка выгрузки YML для Яндекс.Маркета
+          </p>
+          <p className="cases__card-date">22.04.2019</p>
+         </div>
+        </a>
+       </div>
+       <a href="#" className="cases__more"></a>
+      </div>
+     </div>
+    </section>
+    <section className="main__team team">
+     <div className="container">
+      <div className="team__container">
+       <h2 className="team__title"></h2>
+       <ul className="team__list">
+        <li className="team__item">
+         <div className="team__item-visual i-wrap">
+          <img
+           className="team__item-img i-full"
+           src="/assets/img/team-1.webp"
+           alt=""
+           loading="lazy"
+          />
+         </div>
+         <p className="team__item-name">Лёша</p>
+         <p className="team__item-post">
+          руководитель поддержки, планирование задач
+         </p>
+        </li>
+        <li className="team__item">
+         <div className="team__item-visual i-wrap">
+          <img
+           className="team__item-img i-full"
+           src="/assets/img/team-2.webp"
+           alt=""
+           loading="lazy"
+          />
+         </div>
+         <p className="team__item-name">Роман</p>
+         <p className="team__item-post">Роман</p>
+        </li>
+        <li className="team__item">
+         <div className="team__item-visual i-wrap">
+          <img
+           className="team__item-img i-full"
+           src="/assets/img/team-3.webp"
+           alt=""
+           loading="lazy"
+          />
+         </div>
+         <p className="team__item-name">Ирина</p>
+         <p className="team__item-post">
+          менеджер по работе с клинетами, организация оказания услуг
+         </p>
+        </li>
+        <li className="team__item">
+         <div className="team__item-visual i-wrap">
+          <img
+           className="team__item-img i-full"
+           src="/assets/img/team-1.webp"
+           alt=""
+           loading="lazy"
+          />
+         </div>
+         <p className="team__item-name">Даша</p>
+         <p className="team__item-post">SEO, веб-маркетинг</p>
+        </li>
+        <li className="team__item">
+         <div className="team__item-visual i-wrap">
+          <img
+           className="team__item-img i-full"
+           src="/assets/img/team-2.webp"
+           alt=""
+           loading="lazy"
+          />
+         </div>
+         <p className="team__item-name">Сергей</p>
+         <p className="team__item-post">
+          технический директор, 14 лет опыт работы с Drupal
+         </p>
+        </li>
+        <li className="team__item">
+         <div className="team__item-visual i-wrap">
+          <img
+           className="team__item-img i-full"
+           src="/assets/img/team-3.webp"
+           alt=""
+           loading="lazy"
+          />
+         </div>
+         <p className="team__item-name">Вадим</p>
+         <p className="team__item-post">UX/UI дизайн</p>
+        </li>
+       </ul>
       </div>
      </div>
     </section>
