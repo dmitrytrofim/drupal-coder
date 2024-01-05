8c981c327eaf1cc01be68a68d32e7879c14ba0f3
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-01-05 11:37:33 +0300

    +

--- src/components/lang-switcher.jsx
+++ src/components/lang-switcher.jsx
@@ -2,16 +2,25 @@ import { useState } from 'react';
 
 function LangSwitcher() {
  const [toggle, setToggle] = useState('');
+ const [ru, setRu] = useState('RU');
+ const [en, setEn] = useState('EN');
  const toggleLang = () => {
   setToggle(toggle == '' ? 'j-show' : '');
+ };
+ const changeLang = () => {
+  setRu(ru == 'RU' ? 'EN' : 'RU');
+  setEn(en == 'EN' ? 'RU' : 'EN');
+  setToggle('');
  };
 
  return (
   <div className="header__lang lang">
    <button onClick={toggleLang} className={`lang__btn m-main ${toggle}`}>
-    RU
+    {ru}
    </button>
-   <button className={`lang__btn m-toggle ${toggle}`}>EN</button>
+   <button onClick={changeLang} className={`lang__btn m-toggle ${toggle}`}>
+    {en}
+   </button>
   </div>
  );
 }
