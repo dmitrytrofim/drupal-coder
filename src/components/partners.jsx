a674aa80fba84f391e232915cb94c2e559774893
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-01-06 09:31:38 +0300

    +

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -1,41 +1,41 @@
+import { v4 as uuidv4 } from 'uuid';
+
 function Partners() {
  let content = [];
  for (let i = 0; i < 3; i++) {
   content.push(
-   <>
-    <div key={i} className="partners__card">
-     <img
-      className="partners__card-img"
-      src="/assets/img/partner-1.webp"
-      alt=""
-      loading="lazy"
-     />
-    </div>
-    <div key={i} className="partners__card">
-     <img
-      className="partners__card-img"
-      src="/assets/img/partner-2.webp"
-      alt=""
-      loading="lazy"
-     />
-    </div>
-    <div key={i} className="partners__card">
-     <img
-      className="partners__card-img"
-      src="/assets/img/partner-3.webp"
-      alt=""
-      loading="lazy"
-     />
-    </div>
-    <div key={i} className="partners__card">
-     <img
-      className="partners__card-img"
-      src="/assets/img/partner-4.webp"
-      alt=""
-      loading="lazy"
-     />
-    </div>
-   </>
+   <div key={uuidv4()} className="partners__card">
+    <img
+     className="partners__card-img"
+     src="/assets/img/partner-1.webp"
+     alt=""
+     loading="lazy"
+    />
+   </div>,
+   <div key={uuidv4()} className="partners__card">
+    <img
+     className="partners__card-img"
+     src="/assets/img/partner-2.webp"
+     alt=""
+     loading="lazy"
+    />
+   </div>,
+   <div key={uuidv4()} className="partners__card">
+    <img
+     className="partners__card-img"
+     src="/assets/img/partner-3.webp"
+     alt=""
+     loading="lazy"
+    />
+   </div>,
+   <div key={uuidv4()} className="partners__card">
+    <img
+     className="partners__card-img"
+     src="/assets/img/partner-4.webp"
+     alt=""
+     loading="lazy"
+    />
+   </div>
   );
  }
  return <div className="partners__row">{content}</div>;
